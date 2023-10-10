2b9fa0852990a1f08946f375513941b34ddaa3f3
Mateo Nicolas Velasquez Gallardo <mateo.velasquez@udla.edu.ec>
2023-10-10 18:41:41 -0500

    Nueva migración de los dos atributos nuevos de Usuario

---
+++ Data/Migrations/20231010234052_AddAttributesUser.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoginRegister_MateoVelasquez.Data.Migrations
+{
+    public partial class AddAttributesUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserNames",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersistedGrants_ConsumedTime",
+                table: "PersistedGrants",
+                column: "ConsumedTime");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PersistedGrants_ConsumedTime",
+                table: "PersistedGrants");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserNames",
+                table: "AspNetUsers");
+        }
+    }
+}
