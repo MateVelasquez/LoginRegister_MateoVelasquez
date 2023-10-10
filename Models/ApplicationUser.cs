e4689e891a81e314d9b5802f7c286bcf74a517c0
Mateo Nicolas Velasquez Gallardo <mateo.velasquez@udla.edu.ec>
2023-10-10 18:40:14 -0500

    Add project files.

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LoginRegister_MateoVelasquez.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? Name { get; set; }
+
+        public string? UserNames {  get; set; }
+
+    }
+}
